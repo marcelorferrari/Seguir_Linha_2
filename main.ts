9f43bd90a4ee959b055c86363fb69aa7d130d104
marcelorferrari <marcelorferrari@yahoo.com.br>
2023-05-14 18:49:54 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ function Calibrar () {
         . . # . #
         `)
     basic.showNumber(tolerancia)
-    basic.pause(1200)
+    basic.pause(1000)
     basic.showLeds(`
         . . # . .
         . # # . .
@@ -29,9 +29,10 @@ function Calibrar () {
         . # # . .
         . . # . .
         `)
+    basic.pause(1000)
     CalibrarEsquerda = pins.analogReadPin(AnalogPin.P1)
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
-    basic.pause(1200)
+    basic.pause(2000)
     basic.showLeds(`
         . . # . .
         . . # # .
@@ -41,19 +42,20 @@ function Calibrar () {
         `)
     CalibrarDireita = pins.analogReadPin(AnalogPin.P2)
     basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+    basic.pause(2000)
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     OnOff()
 })
 input.onButtonPressed(Button.A, function () {
-    tolerancia = tolerancia + 10
+    tolerancia = tolerancia + 50
     basic.showNumber(tolerancia)
 })
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     reiniciar()
 })
 input.onButtonPressed(Button.B, function () {
-    tolerancia = tolerancia - 10
+    tolerancia = tolerancia - 50
     basic.showNumber(tolerancia)
 })
 function mover () {
@@ -105,7 +107,7 @@ function mover () {
 }
 function reiniciar () {
     ligado = 0
-    tolerancia = 30
+    tolerancia = 250
     robotbit.MotorRun(robotbit.Motors.M1A, 0)
     robotbit.MotorRun(robotbit.Motors.M2B, 0)
     Calibrar()
@@ -123,5 +125,5 @@ let CalibrarEsquerda = 0
 let tolerancia = 0
 reiniciar()
 basic.forever(function () {
-	
+    mover()
 })
