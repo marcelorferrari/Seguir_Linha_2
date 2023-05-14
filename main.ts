731246f2f6d83e44f12ceb248169fbe35e049bad
marcelorferrari <marcelorferrari@yahoo.com.br>
2023-05-14 17:40:13 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,15 +1,52 @@
+function Calibrar (cal: number) {
+    basic.showLeds(`
+        # . # . .
+        . # . . .
+        # . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    tolerancia = tolerancia + cal
+    basic.showNumber(tolerancia)
+    basic.pause(1200)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        # # # # #
+        . # # . .
+        . . # . .
+        `)
+    CalibrarEsquerda = pins.analogReadPin(AnalogPin.P1)
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.pause(1200)
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        # # # # #
+        . . # # .
+        . . # . .
+        `)
+    CalibrarDireita = pins.analogReadPin(AnalogPin.P2)
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (ligado == 0) {
-        ligado = 1
-    } else {
-        ligado = 0
-    }
+    OnOff()
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    Calibrar(10)
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    reiniciar()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+    Calibrar(-10)
 })
 function mover () {
     basic.showLeds(`
@@ -20,7 +57,7 @@ function mover () {
         . . . . .
         `)
     if (ligado == 1) {
-        if (pins.analogReadPin(AnalogPin.P1) >= 100 && pins.analogReadPin(AnalogPin.P2) <= 900) {
+        if (pins.analogReadPin(AnalogPin.P1) <= CalibrarDireita - tolerancia) {
             basic.showLeds(`
                 . . # . .
                 . . # # .
@@ -31,7 +68,7 @@ function mover () {
             robotbit.MotorRun(robotbit.Motors.M1A, 0)
             robotbit.MotorRun(robotbit.Motors.M2B, 75)
             basic.pause(50)
-        } else if (pins.analogReadPin(AnalogPin.P2) >= 100 && pins.analogReadPin(AnalogPin.P1) <= 900) {
+        } else if (pins.analogReadPin(AnalogPin.P2) <= CalibrarEsquerda - tolerancia) {
             basic.showLeds(`
                 . . # . .
                 . # # . .
@@ -58,12 +95,25 @@ function mover () {
         robotbit.MotorRun(robotbit.Motors.M2B, 0)
     }
 }
+function reiniciar () {
+    ligado = 0
+    tolerancia = 30
+    robotbit.MotorRun(robotbit.Motors.M1A, 0)
+    robotbit.MotorRun(robotbit.Motors.M2B, 0)
+    Calibrar(0)
+}
+function OnOff () {
+    if (ligado == 0) {
+        ligado = 1
+    } else {
+        ligado = 0
+    }
+}
 let ligado = 0
-let LCal = pins.analogReadPin(AnalogPin.P1)
-let RCal = pins.analogReadPin(AnalogPin.P2)
-ligado = 0
-robotbit.MotorRun(robotbit.Motors.M1A, 0)
-robotbit.MotorRun(robotbit.Motors.M2B, 0)
+let CalibrarDireita = 0
+let CalibrarEsquerda = 0
+let tolerancia = 0
+reiniciar()
 basic.forever(function () {
 	
 })
